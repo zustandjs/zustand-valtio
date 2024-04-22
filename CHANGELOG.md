dee407e53e37be36a8661b1deb2a8e617bd782c2
daishi <daishi@axlight.com>
2024-04-22 10:09:52 +0900

    module-first setup

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -2,6 +2,12 @@
 
 ## [Unreleased]
 
+### Changed
+
+- Module-first setup
+
 ## [0.0.1] - 2024-03-25
+
 ### Added
+
 - Initial release
--- __tests__/01_basic_spec.tsx
+++
@@ -1,7 +0,0 @@
-import { createWithProxy } from '../src/index';
-
-describe('basic spec', () => {
-  it('should export functions', () => {
-    expect(createWithProxy).toBeDefined();
-  });
-});
