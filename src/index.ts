edef2914b391132405a03aee2e7e0385972ac74a
Daishi Kato <dai-shi@users.noreply.github.com>
2024-07-14 22:46:01 +0900

    feat: support nested objects and slice pattern (#7)
    * refactor
    
    * applyChanges from jotai-valtio
    
    * patch function recursively
    
    * examples/04
    
    * update CHANGELOG & README
    
    * refactor

--- src/index.ts
+++ src/index.ts
@@ -1,15 +1,20 @@
 import type { StateCreator, StoreApi, StoreMutatorIdentifier } from 'zustand';
 import { proxy, snapshot, subscribe } from 'valtio/vanilla';
 
+type AnyFunction = (...args: never[]) => unknown;
+
+type DeepWritable<T> = T extends object
+  ? { -readonly [K in keyof T]: DeepWritable<T[K]> }
+  : T;
+
 // TODO replace with Valtio's Snapshot type in v2
-type Snapshot<T> = T extends (...args: never[]) => unknown
+type Snapshot<T> = T extends AnyFunction
   ? T
   : T extends object
     ? { readonly [K in keyof T]: Snapshot<T[K]> }
     : T;
 
 type StoreWithProxy<T> = {
-  setState: never;
   getProxyState: () => T;
 };
 
@@ -18,10 +23,6 @@ type Write<T, U> = Omit<T, keyof U> & U;
 type WithWithProxy<S, _A> = S extends { getState: () => Snapshot<infer T> }
   ? Write<S, StoreWithProxy<DeepWritable<T>>>
   : never;
-
-type DeepWritable<T> = T extends object
-  ? { -readonly [K in keyof T]: DeepWritable<T[K]> }
-  : T;
 
 declare module 'zustand/vanilla' {
   interface StoreMutators<S, A> {
@@ -41,8 +42,51 @@ type WithProxyImpl = <T extends object>(
   initialObject: T,
 ) => StateCreator<Snapshot<T>, [], []>;
 
+const isObject = (x: unknown): x is object =>
+  typeof x === 'object' && x !== null && !(x instanceof Promise);
+
+const patchObjectMethods = <T extends object>(
+  proxyObject: T,
+  fn: (obj: object, key: string, fn: AnyFunction) => void,
+) => {
+  (Object.getOwnPropertyNames(proxyObject) as (keyof T)[]).forEach((key) => {
+    const value = proxyObject[key];
+    if (typeof value === 'function') {
+      fn(proxyObject, key as string, value as AnyFunction);
+    } else if (isObject(value)) {
+      patchObjectMethods(value, fn);
+    }
+  });
+};
+
+const applyChanges = <T extends object>(proxyObject: T, prev: T, next: T) => {
+  (Object.getOwnPropertyNames(prev) as (keyof T)[]).forEach((key) => {
+    if (!(key in next)) {
+      delete proxyObject[key];
+    } else if (Object.is(prev[key], next[key])) {
+      // unchanged
+    } else if (
+      isObject(proxyObject[key]) &&
+      isObject(prev[key]) &&
+      isObject(next[key])
+    ) {
+      applyChanges(
+        proxyObject[key] as unknown as object,
+        prev[key] as unknown as object,
+        next[key] as unknown as object,
+      );
+    } else {
+      proxyObject[key] = next[key];
+    }
+  });
+  (Object.keys(next) as (keyof T)[]).forEach((key) => {
+    if (!(key in prev)) {
+      proxyObject[key] = next[key];
+    }
+  });
+};
+
 const withProxyImpl: WithProxyImpl = (initialObject) => (set, get, api) => {
-  type AnyObject = Record<string, unknown>;
   const proxyState = proxy(initialObject);
   let mutating = 0;
   let updating = 0;
@@ -53,37 +97,30 @@ const withProxyImpl: WithProxyImpl = (initialObject) => (set, get, api) => {
       --updating;
     }
   };
-  Object.keys(proxyState).forEach((key) => {
-    const fn = (proxyState as AnyObject)[key];
-    // TODO this doesn't handle nested objects
-    if (typeof fn === 'function') {
-      (proxyState as AnyObject)[key] = (...args: never[]) => {
+  patchObjectMethods(proxyState, (obj, key, fn) => {
+    Object.defineProperty(obj, key, {
+      value: (...args: never[]) => {
         try {
           ++mutating;
-          return fn.apply(proxyState, args);
+          return fn.apply(obj, args);
         } finally {
           --mutating;
           updateState();
         }
-      };
-    }
+      },
+    });
   });
   type Api = StoreApi<unknown> & StoreWithProxy<typeof initialObject>;
-  delete (api as Api).setState;
   (api as Api).getProxyState = () => proxyState;
   subscribe(proxyState, updateState, true);
   api.subscribe(() => {
     if (!updating) {
       // HACK for persist hydration
-      const state = get() as AnyObject;
-      Object.keys(state).forEach((key) => {
-        const val = state[key];
-        // TODO this doesn't handle nested objects
-        if (typeof val !== 'function') {
-          // XXX this will throw if val is a snapshot
-          (proxyState as AnyObject)[key] = val;
-        }
-      });
+      applyChanges(
+        proxyState,
+        snapshot(proxyState) as typeof proxyState,
+        get() as typeof proxyState,
+      );
     }
   });
   return snapshot(proxyState) as Snapshot<typeof proxyState>;
